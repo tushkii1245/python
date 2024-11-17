23a8655bb14b4a8794fd71a75dd07367e6058d09
Tushar Patil <tusharpatil@192.168.0.37>
2024-11-17 23:23:51 +0530

    print fn

---
+++ 1.helloworld.py
@@ -0,0 +1,23 @@
+print ("hello")
+
+print(5)
+
+print(5.6)
+
+print("hello","world")
+
+
+print("india","aus","nz")
+#
+print("india","aus","nz",sep=' ')
+#seperate by /
+print("india","aus","nz",sep='/')
+#separate by -
+print("india","aus","nz",sep='-')
+
+print("hello")
+print("woorld")
+#print two op in same line
+print("hello",end=' ')
+print("woorld")
+
